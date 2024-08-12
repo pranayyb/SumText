f7f295d05714697e100dbd4383328623511efe84
Pranay Buradkar <pranayyb@Pranays-Air.local>
2024-08-12 23:16:38 +0530

    Model file

---
+++ summarizer.py
@@ -0,0 +1,54 @@
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest
+# from spacy.pipeline.textacy import Textacy
+
+
+# def extract_keywords(rawdocs):
+#     nlp = spacy.load('en_core_web_sm')
+#     doc = nlp(rawdocs)
+#     keywords = [chunk.text for chunk in doc.noun_chunks]
+#     return keywords
+    
+def summarizer(rawdocs):
+    stopwords=list(STOP_WORDS)
+    
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(rawdocs)
+    # extract_keywords(rawdocs,doc)
+    
+    tokens=[token.text for token in doc]
+    
+    word_freq = {}
+    for word in doc:
+        if word.text.lower() not in stopwords and word.text.lower() not in punctuation:
+            if word.text not in word_freq.keys():
+                word_freq[word.text]=1
+            else:
+                word_freq[word.text]+=1
+    
+    max_freq=max(word_freq.values())
+    for word in word_freq.keys():
+        word_freq[word]=word_freq[word]/max_freq
+    
+    sent_tokens  =[sent for sent in doc.sents]
+    
+    sent_scores = {}
+    for sent in sent_tokens:
+        for word in sent:
+            if word.text in word_freq.keys():
+                if sent not in sent_scores.keys():
+                    sent_scores[sent] =word_freq[word.text]
+                else:
+                    sent_scores[sent]+=word_freq[word.text]
+    
+    select_len=int(len(sent_tokens)*0.25)
+    
+    summary = nlargest(select_len, sent_scores,key=sent_scores.get)
+    final_summary=[word.text for word in summary]
+    summary = ' '.join(final_summary)
+
+    return summary, doc,len(rawdocs.split(' ')),len(summary.split(' '))
+    
+    
